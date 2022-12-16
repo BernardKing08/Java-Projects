ec11f55783cd9084c79e391e00f6834ce79f15f5
BernardKing08 <bernardkingsley08@gmail.com>
2022-12-16 20:48:32 +0100

    Code runs from here

---
+++ Abstraction_concept/Parent/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args){
+        subclass1 user = new subclass1(); 
+        subclass2 user1 = new subclass2(); 
+
+        user.message();
+        user1.message();
+    }
+}
