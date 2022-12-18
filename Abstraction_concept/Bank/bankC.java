e140190eee589887a3ef8b00ba482e7bd0e66fb5
BernardKing08 <bernardkingsley08@gmail.com>
2022-12-18 17:02:58 +0100

    Subclass of the abstract class bank

---
+++ Abstraction_concept/Bank/bankC.java
@@ -0,0 +1,11 @@
+public class bankC extends bank{
+    int balance; 
+
+    bankC(int balance){
+        this.balance = balance; 
+    }
+    
+    public int getbalance(){
+        return balance;
+    }
+}
