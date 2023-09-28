38e97e9b89e8bf01dd753adf7f4ff49bf8bef9f5
BernardKing08 <BernardKingsley08@gmail.com>
2023-09-28 22:29:48 +0100

    created the edit class

---
+++ Projects/Notepad/Function_Edit.java
@@ -0,0 +1,16 @@
+//class in progress 
+public class Function_Edit {
+
+    //an object instance of the notepad class to be used
+    Notepad gui; 
+
+    
+    Function_Edit(Notepad gui){
+        this.gui = gui;
+    }
+
+    public void copy(){
+
+    }
+    
+}//end of Edit class 
