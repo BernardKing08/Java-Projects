fb5531644fd98c2c320db6d5dbc025a0ce665de1
BernardKing08 <BernardKingsley08@gmail.com>
2023-09-28 22:30:21 +0100

    added comments to the Main class

--- Projects/Notepad/Main.java
+++ Projects/Notepad/Main.java
@@ -1,13 +1,16 @@
-package testinglab;
-
 public class Main {
+    //code runs from here
     public static void main(String[] args){
+         
         Notepad Gui = new Notepad();
         
         Gui.createMenuBar();
         Gui.createfileMenu();
+        Gui.createFormatMenu();
         Gui.createTextArea();
         Gui.go(); 
         
-    }
-}
+    }//end of main method 
+
+    
+}//end of main class
