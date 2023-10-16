41b2a39cdca9e4aa3de0b9dffd14f9251667eb3b
BernardKing08 <BernardKingsley08@gmail.com>
2023-10-16 21:49:37 +0100

    added colors, undo and redo feature

--- Projects/Notepad/Function_Edit.java
+++ Projects/Notepad/Function_Edit.java
@@ -1,16 +1,40 @@
-//class in progress 
-public class Function_Edit {
 
+import java.awt.Color;
+
+//work im progress
+
+public class Function_Edit {
     //an object instance of the notepad class to be used
-    Notepad gui; 
+    Notepad gui;
 
     
     Function_Edit(Notepad gui){
         this.gui = gui;
     }
 
-    public void copy(){
+    //method to change the color of the text inn textArea
+    public void changeColor(String str){
 
+        //switch statement to alternate between colors
+        switch(str){
+            case "WHITE": gui.textArea.setForeground(Color.WHITE);
+                break; 
+            case "BLUE": gui.textArea.setForeground(Color.BLUE);
+                break; 
+            case "BLACK": gui.textArea.setForeground(Color.BLACK);
+                break; 
+        }
+    }//end of changeColor method
+
+    //undo command 
+    public void undo(){
+        gui.um.undo();
+    }
+
+    //redo comand 
+    public void redo(){
+        gui.um.redo();
     }
     
-}//end of Edit class 
+
+}
