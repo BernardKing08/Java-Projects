4de3a17c95a4144eb960dd236919b5b05368a265
BernardKing08 <BernardKingsley08@gmail.com>
2023-10-16 21:59:43 +0100

    Added the edit menu function

--- Projects/Notepad/Main.java
+++ Projects/Notepad/Main.java
@@ -1,16 +1,18 @@
 public class Main {
-    //code runs from here
+    
     public static void main(String[] args){
-         
+        //code runs from here 
         Notepad Gui = new Notepad();
+        
         
         Gui.createMenuBar();
         Gui.createfileMenu();
+        Gui.createEditMenu();
         Gui.createFormatMenu();
         Gui.createTextArea();
         Gui.go(); 
         
-    }//end of main method 
+    }//end of main method
 
     
 }//end of main class
