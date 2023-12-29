98f53948321424e95e07421f612578c36cdf42bc
BernardKing08 <BernardKingsley08@gmail.com>
2023-12-29 17:33:02 +0100

    Code runs from here

---
+++ Projects/Tic-Tac-Toe/Main.java
@@ -0,0 +1,9 @@
+package TicTacToe;
+
+public class Main {
+    public static void main(String[] args){
+        
+        //running the tictactoe application
+        TicTacToe go = new TicTacToe(); 
+    }
+}
