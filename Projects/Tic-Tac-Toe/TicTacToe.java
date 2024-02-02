12f33cb1deac53d649ed0c48aa8aaecdc124c9bd
kingsley Bernard <bernardkingsley08@gmail.com>
2024-02-02 22:49:46 +0100

    A Tictactoe game using java and java swing

--- Projects/Tic-Tac-Toe/TicTacToe.java
+++ Projects/Tic-Tac-Toe/TicTacToe.java
@@ -53,6 +53,23 @@ public class TicTacToe implements ActionListener{
 
     }
 
+    //adding buttons to the GridPanel
+    public void addButtons(){
+        //creating JButton array
+        button = new JButton[9];
+    
+        //button component
+        for(int i = 0; i < 9; i++){
+            button[i] = new JButton();
+            button[i].setFocusable(false);
+            button[i].setForeground(Color.cyan);
+            button[i].setBackground(Color.BLACK);
+            button[i].setFont(new Font("Monospaced", Font.BOLD,70));
+            button[i].addActionListener(this);
+            panel2.add(button[i]); 
+        }
+    }
+
     //to shuffle between the players 
     private void userTurn(){
         //to delay the main thread
@@ -72,28 +89,6 @@ public class TicTacToe implements ActionListener{
             playerTurn = false; 
             label.setText("O turn");
         }
-    }
-
-    //adding buttons to the GridPanel
-    public void addButtons(){
-        //creating JButton array
-        button = new JButton[9];
-    
-        //button component
-        for(int i = 0; i < 9; i++){
-            button[i] = new JButton();
-            button[i].setFocusable(false);
-            button[i].setForeground(Color.cyan);
-            button[i].setBackground(Color.BLACK);
-            button[i].setFont(new Font("Monospaced", Font.BOLD,70));
-            button[i].addActionListener(this);
-            panel2.add(button[i]); 
-        }
-    }
-
-    //to check if the user makes a winning move 
-    public void check(){
-        //work in progress
     }
 
     //to control the button interface
@@ -116,10 +111,115 @@ public class TicTacToe implements ActionListener{
                         button[i].setText("O"); 
                         label.setText("X turn");
                         playerTurn = true;
+                        check();
                     }
                 }
             }
         }
     }//end of method
+
+    //to check if the user, either X or O makes a winning move 
+    public void check(){
+        //checking if X wins
+        if(     (button[0].getText() == "X") && 
+                (button[1].getText() == "X") &&
+                (button[2].getText() == "X")){
+                    xWins(0,1,2); 
+        }
+        if(     (button[3].getText() == "X") &&
+                (button[4].getText() == "X") &&
+                (button[5].getText() == "X")){
+                    xWins(3,4,5); 
+        }
+        if(     (button[6].getText() == "X") &&    
+                (button[7].getText() == "X") && 
+                (button[8].getText() == "X")){
+                    xWins(6,7,8); 
+        }
+        if(     (button[1].getText() == "X") &&    
+                (button[4].getText() == "X") && 
+                (button[7].getText() == "X")){
+                    xWins(1,4,7);
+        } 
+        if(     (button[2].getText() == "X") &&    
+                (button[5].getText() == "X") && 
+                (button[8].getText() == "X")){
+                    xWins(2,5,8);
+        } 
+        if(     (button[0].getText() == "X") &&    
+                (button[4].getText() == "X") && 
+                (button[8].getText() == "X")){
+                    xWins(0,4,8);
+        } 
+        if(     (button[2].getText() == "X") &&    
+                (button[4].getText() == "X") && 
+                (button[6].getText() == "X")){
+                    xWins(2,4,6);
+        } 
+
+        //checking winning condition for 'O'
+        if(     (button[0].getText() == "O") && 
+                (button[1].getText() == "O") &&
+                (button[2].getText() == "O")){
+                    OWins(0,1,2); 
+        }
+        if(     (button[3].getText() == "O") &&
+                (button[4].getText() == "O") &&
+                (button[5].getText() == "O")){
+                    OWins(3,4,5); 
+        }
+        if(     (button[6].getText() == "O") &&    
+                (button[7].getText() == "O") && 
+                (button[8].getText() == "O")){
+                    OWins(6,7,8); 
+        }
+        if(     (button[1].getText() == "O") &&    
+                (button[4].getText() == "O") && 
+                (button[7].getText() == "O")){
+                    OWins(1,4,7);
+        } 
+        if(     (button[2].getText() == "O") &&    
+                (button[5].getText() == "O") && 
+                (button[8].getText() == "O")){
+                    OWins(2,5,8);
+        } 
+        if(     (button[0].getText() == "O") &&    
+                (button[4].getText() == "O") && 
+                (button[8].getText() == "O")){
+                    OWins(0,4,8);
+        } 
+        if(     (button[2].getText() == "O") &&    
+                (button[4].getText() == "O") && 
+                (button[6].getText() == "O")){
+                    OWins(2,4,6);
+        } 
+
+    }
+
+    public void xWins(int num, int num2, int num3){
+        button[num].setBackground(Color.green);
+        button[num2].setBackground(Color.green);
+        button[num3].setBackground(Color.green);
+
+        //to disable the buttons 
+        for(JButton butt : button){
+            butt.setEnabled(false);
+        }
+
+        label.setText("X wins");
+    }
+
+    public void OWins(int num, int num2, int num3){
+        button[num].setBackground(Color.green);
+        button[num2].setBackground(Color.green);
+        button[num3].setBackground(Color.green);
+
+        for(JButton butt : button){
+            butt.setEnabled(false);
+        }
+
+        label.setText("O wins");
+    }
+
 
 }//end of class
