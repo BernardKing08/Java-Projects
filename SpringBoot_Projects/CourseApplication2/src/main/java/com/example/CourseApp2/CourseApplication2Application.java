e73f6c634f4e4a3a608148ed8bdb91cd2e14a4de
BernardKing08 <BernardKingsley08@gmail.com>
2024-04-11 16:40:06 +0100

    Code runs from here

---
+++ SpringBoot_Projects/CourseApplication2/src/main/java/com/example/CourseApp2/CourseApplication2Application.java
@@ -0,0 +1,14 @@
+package com.example.CourseApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//code runs from here
+public class CourseApplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication2Application.class, args);
+	}
+
+}
