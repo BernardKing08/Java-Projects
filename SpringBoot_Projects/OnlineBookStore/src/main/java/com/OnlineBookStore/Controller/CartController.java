3de59f9b516c06b6bceef07030c6e6f4102b9c1b
kingsley Bernard <bernardkingsley08@gmail.com>
2024-07-01 19:52:00 +0100

    Added the /remove path and made changes to the /addToCart path

--- SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Controller/CartController.java
+++ SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Controller/CartController.java
@@ -1,33 +1,54 @@
 package com.OnlineBookStore.Controller;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.OnlineBookStore.Model.Book;
+import com.OnlineBookStore.Model.Cart;
+import com.OnlineBookStore.Service.BookService;
 import com.OnlineBookStore.Service.CartService;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class CartController {
 
-	private final CartService cartService;
-	
-	CartController(CartService cartService){
-		this.cartService = cartService;
-		
-	}
-	
-	@PostMapping("/addToCart")
-    public String addToCart(@ModelAttribute Book book, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    private final CartService cartService;
+    private final BookService bookService;
+
+    CartController(CartService cartService, BookService bookService) {
+        this.cartService = cartService;
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/cart")
+    public String displayCart(Model model) {
+        List<Cart> cartItems = cartService.getCart();
+        
+        System.out.println("Items in cart: " + cartItems); // Debugging statement
+        
+        model.addAttribute("cartItems", cartItems);
+        return "cart";
+    }
+
+    @PostMapping("/addToCart")
+    public String addToCart(@RequestParam("id") String bookId, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        Book book = bookService.getBook(bookId);
+        System.out.println("Adding book to cart: " + book); // Debugging statement
         cartService.addBook(book);
-
-        // Capture the referring URL
-        String referer = request.getHeader("Referer");
-
-        // Redirect back to the referring URL
+        
+        String referer = request.getHeader("Referer"); //gets current page url and refreshes back to it i.e /cart
         return "redirect:" + referer;
+    }
+
+    @PostMapping("/removeItem")
+    public String removeItem(@RequestParam("id") int cartItemNo) {
+        System.out.println("Removing cart item: " + cartItemNo); // Debugging statement
+        cartService.removeBook(cartItemNo);
+        return "redirect:/cart";
     }
 }
