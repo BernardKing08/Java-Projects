87147d72b53860084c1f93a4e34e5e37cf712d90
kingsley Bernard <bernardkingsley08@gmail.com>
2024-07-01 19:50:31 +0100

    Made changes to paths in the controller

--- SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Controller/BController.java
+++ SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Controller/BController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BController {
 		
-	@RequestMapping(value = "/cart")
+	/*
+	 *@RequestMapping(value = "/cart")
 	public String getcarts() {
 		return "cart"; 
-	}
+	} 
+	 * */
+	
 
 }
