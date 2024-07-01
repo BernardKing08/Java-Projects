2d3b2367db97c98515fc148e35ff32d166c53c04
kingsley Bernard <bernardkingsley08@gmail.com>
2024-07-01 19:56:58 +0100

    Created business side logic for /removeBook and /addToCart - also removing a specific book using a randomNumber serving as UUID for the book

--- SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Service/CartService.java
+++ SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Service/CartService.java
@@ -1,16 +1,34 @@
 package com.OnlineBookStore.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
-
 import com.OnlineBookStore.Model.Book;
+import com.OnlineBookStore.Model.Cart;
 
 @Service
 public class CartService {
 
-	public void addBook(Book book) {
-		
+	private List<Cart> cartBook = new ArrayList<>();
+
+    public void addBook(Book book) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((1000 - 0) + 1) + 1; // generating a random number to serve as the UUId for now 
+        cartBook.add(new Cart(book, randomNum));
+    }
+	
+	public List<Cart> getCart() {
+		return cartBook;
 		
 	}
+
+	public void removeBook(int cartItemNo) {
+        cartBook = cartBook.stream()
+            .filter(cart -> cart.getCartItemNo() != cartItemNo)
+            .collect(Collectors.toList());
+    }
 
 	
 }
