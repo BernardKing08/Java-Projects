bb78d6ca59cadfae3ef083005c9e78ca95b8a4fe
kingsley Bernard <bernardkingsley08@gmail.com>
2024-07-07 19:58:39 +0100

    Added Bookstrap 5 pluggin to the javascript file

--- SpringBoot_Projects/OnlineBookStore/src/main/resources/static/assets/js/scripts.js
+++ SpringBoot_Projects/OnlineBookStore/src/main/resources/static/assets/js/scripts.js
@@ -1,5 +1,9 @@
 // scripts.js
-
+const bootstrapScript = document.createElement('script');
+bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+bootstrapScript.integrity = "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz";
+bootstrapScript.crossOrigin = "anonymous";
+document.head.appendChild(bootstrapScript);
 // Sample script to handle adding items to the cart and other interactivity
 
 document.addEventListener('DOMContentLoaded', function () {
