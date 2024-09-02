0ffe58fe627eea3e1080306227c5057be5c977f4
kingsley Bernard <bernardkingsley08@gmail.com>
2024-09-02 15:51:24 +0100

    Enabled adding and removing of books using the book uuid instead

--- SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Service/CartService.java
+++ SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Service/CartService.java
@@ -2,8 +2,8 @@ package com.OnlineBookStore.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.OnlineBookStore.Model.Book;
 import com.OnlineBookStore.Model.Cart;
@@ -11,22 +11,45 @@ import com.OnlineBookStore.Model.Cart;
 @Service
 public class CartService {
 
-	private List<Cart> cartBook = new ArrayList<>();
-
-    public void addBook(Book book) {
-        Random rand = new Random();
-        int randomNum = rand.nextInt((1000 - 0) + 1) + 1; // generating a random number to serve as the UUId for now 
-        cartBook.add(new Cart(book, randomNum));
-    }
+	//an arrayList of book 
+    //private List<Book> cartBooks = new ArrayList<>();
 	
-	public List<Cart> getCart() {
-		return cartBook;
-		
-	}
+	//getting an instance of cart pojo
+	@Autowired
+	private Cart cart; 
 
-	public void removeBook(int cartItemNo) {
-        cartBook = cartBook.stream()
-            .filter(cart -> cart.getCartItemNo() != cartItemNo)
-            .collect(Collectors.toList());
+  
+    // Method to add a book to the cart
+    public void addBook(Book book) {
+    	book.setQty(book.getQty() + 1); //increasing the book by 1
+    	
+    	if(cart.getBooksInCart().contains(book)) {
+    		cart.getBooksInCart().set(cart.getBooksInCart().indexOf(book), book); //adding the book to the cart list 
+    	}
+    	else {
+    		cart.getBooksInCart().add(book);
+    	}
+    }
+
+    public void removeBook(String uuid) {
+        // Find the book in the cart by UUID using streams
+        cart.getBooksInCart().stream()
+            .filter(book -> book.getUuid().equals(uuid))
+            .findFirst()
+            .ifPresent(book -> {
+                if (book.getQty() > 1) {
+                    // Decrease the quantity if more than 1
+                    book.setQty(book.getQty() - 1);
+                } else {
+                    // Remove the book if quantity is 1 or less
+                    cart.getBooksInCart().remove(book);
+                }
+            });
+    }
+
+
+    // Method to get all books in the cart
+    public List<Book> getCart() {
+        return cart.getBooksInCart();
     }
 }
