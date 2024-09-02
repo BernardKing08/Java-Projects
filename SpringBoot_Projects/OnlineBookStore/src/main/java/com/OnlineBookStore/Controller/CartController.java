f4d27fd32ce9a23b359bda4d2be7a521bacb3578
kingsley Bernard <bernardkingsley08@gmail.com>
2024-09-02 15:52:25 +0100

    worked on the /remove controller

--- SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Controller/CartController.java
+++ SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Controller/CartController.java
@@ -6,11 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.OnlineBookStore.Model.Book;
 import com.OnlineBookStore.Model.Cart;
 import com.OnlineBookStore.Service.BookService;
 import com.OnlineBookStore.Service.CartService;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -26,28 +26,24 @@ public class CartController {
 
     @RequestMapping("/cart")
     public String displayCart(Model model) {
-        List<Cart> cartItems = cartService.getCart();
-        
-        System.out.println("Items in cart: " + cartItems); // Debugging statement
+        List<Book> cartItems = cartService.getCart();
         
         model.addAttribute("cartItems", cartItems);
         return "cart";
     }
 
     @PostMapping("/addToCart")
-    public String addToCart(@RequestParam("id") String bookId, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    public String addToCart(@RequestParam("id") String bookId, HttpServletRequest request) {
         Book book = bookService.getBook(bookId);
-        System.out.println("Adding book to cart: " + book); // Debugging statement
         cartService.addBook(book);
         
-        String referer = request.getHeader("Referer"); //gets current page url and refreshes back to it i.e /cart
+        String referer = request.getHeader("Referer");
         return "redirect:" + referer;
     }
 
     @PostMapping("/removeItem")
-    public String removeItem(@RequestParam("id") int cartItemNo) {
-        System.out.println("Removing cart item: " + cartItemNo); // Debugging statement
-        cartService.removeBook(cartItemNo);
+    public String removeItem(@RequestParam("id") String bookId) {
+        cartService.removeBook(bookId);
         return "redirect:/cart";
     }
 }
