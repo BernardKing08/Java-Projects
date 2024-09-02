7a4219b347461031077b38c5a0e3b812b6c1576c
kingsley Bernard <bernardkingsley08@gmail.com>
2024-09-02 15:47:14 +0100

    Added a Price and uuid for each book respectively

--- SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Model/Book.java
+++ SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Model/Book.java
@@ -1,13 +1,13 @@
 package com.OnlineBookStore.Model;
 
+import java.util.UUID;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Book {
 	
@@ -18,6 +18,9 @@ public class Book {
 	@Size(min = 3)
 	private String name; 
 	
+	//uuid for keeping track of books in cart
+	private String uuid;
+
 	private String imageUrl;
 	
 	@NotBlank(message = "author must not be blank")
@@ -27,5 +30,24 @@ public class Book {
 	@NotBlank(message = "description must not be blank")
 	@Size(min = 10)
 	private String description;
+	
+	//keeps track of the nummber of book-items in the cart
+	private int qty;
+	
+	//price of the item
+	private double price;
+	
+	public Book(@NotBlank(message = "id must not be blank") String id,
+				@NotBlank(message = "name must not be blank") @Size(min = 3) String name, String imageUrl,
+				@NotBlank(message = "author must not be blank") @Size(min = 5) String author,
+				@NotBlank(message = "description must not be blank") @Size(min = 10) String description) {
+		this.id = id;
+		this.uuid = UUID.randomUUID().toString();
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.author = author;
+		this.description = description;
+		this.qty = 0;
+	}
 	
 }
