4e64c2ad60c2c406ee11148da04277eddb4d6a3d
kingsley Bernard <bernardkingsley08@gmail.com>
2024-09-02 15:48:29 +0100

    cart pojo class

--- SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Model/Cart.java
+++ SpringBoot_Projects/OnlineBookStore/src/main/java/com/OnlineBookStore/Model/Cart.java
@@ -1,13 +1,21 @@
 package com.OnlineBookStore.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+@Component
 public class Cart {
 	
-	private Book book;
-	private int cartItemNo;
+	//create an arrayList of books
+	private List<Book> booksInCart = new ArrayList<>();
+	private double totalPrice;
+	
+	
 }
 
