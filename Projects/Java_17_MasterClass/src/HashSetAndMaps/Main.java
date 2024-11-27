00a6097e3b9422c8e02014e4e133e09dc80676bd
BernardKing08 <bernardkingsley08@gmail.com>
2024-11-27 23:08:45 +0100

    Project using Sets to sort Contacts

--- Projects/Java_17_MasterClass/src/HashSetAndMaps/Main.java
+++ Projects/Java_17_MasterClass/src/HashSetAndMaps/Main.java
@@ -1,7 +1,39 @@
 package HashSetAndMaps;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+
+        List<Contact> emails =  ContactData.getData("email");
+        List<Contact> phones = ContactData.getData("phone");
+        printData("Phone List", phones);
+        printData("Email List", emails);
+
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+        printData("Phone Contacts", phoneContacts);
+        printData("Email Contacts", emailContacts);
+
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com",
+        "RHood@sherwoodforest.org");
+        System.out.println(robinHood);
+
+    }
+
+    public static void printData(String header, Collection<Contact> contacts) {
+
+        System.out.println("----------------------------------------------");
+        System.out.println(header);
+        System.out.println("----------------------------------------------");
+        contacts.forEach(System.out::println);
     }
 }
